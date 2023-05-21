75600a5481dbc95459b0692bcfa41774b5510cb5
Mr. Hood <adamhood15@gmail.com>
2023-05-21 18:20:32 -0500

    Added delete and update routes for each api file

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -50,10 +50,10 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      "product_name": "Basketball",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
@@ -123,8 +123,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productTagData = await ProductTag.destroy({
+      where: { product_id: req.params.id}
+    });
+
+    const productData = await Product.destroy({
+      where: {id: req.params.id}
+    });
+    res.status(200).json(productData);
+  
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
